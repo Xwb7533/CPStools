5a99a1c12ae1a73bfd8b19bb1e2d5094e5402f8a
Xu Wenbo <93017074+Xwb7533@users.noreply.github.com>
2024-07-03 08:01:09 +0800

    Update LSR_annotation.py

--- src/LSR_annotation.py
+++ src/LSR_annotation.py
@@ -120,37 +120,57 @@ def IGS_extract(input_file):
         final_results = os.path.join(os.path.dirname(input_file), final_name)
         data = []
         st_lines2 = open(file1, 'r').readlines()[1:]
+        unique_data = set()
+        # 处理5列数据的部分
         for line in st_lines:
             parts = line.strip().split('\t')
-            name, start, end, loc = parts[0], int(parts[1]), int(parts[2]), parts[-1]
-            data.append((name, start, end, loc))
-        for line in st_lines:
-            parts = line.strip().split('\t')
-            name, start, end, loc = parts[0], int(parts[1]), int(parts[2]), parts[-1]
-            data.append((name, start, end, loc))
+            name = parts[0]
+            start = int(parts[1])
+            end = int(parts[2])
+            loc = parts[-1]
 
-        printed_combinations = {}
+            # 处理额外的范围
+            if len(parts) == 6:
+                extra_start = int(parts[3])
+                extra_end = int(parts[4])
+                unique_data.add((name, start, end, loc))
+                unique_data.add((name, extra_start, extra_end, loc))
+            else:
+                unique_data.add((name, start, end, loc))
+
+        data = list(unique_data)
+        
+        printed_combinations = set()
+        # write output results
         with open(final_results, 'w') as ff:
+            ff.write("type\tlength\tstart\tend\tloc\tloc_type\n")
             for i in st_lines2:
-                st_list = re.split('\s+', i.strip())
-                st_join = '\t'.join(info.strip() for info in st_list)
-                start_point = int(st_list[1])
-                length = int(st_list[0])
-                # 将 label 的列表转换为元组
-                label = tuple(st_list[:7])  # 这里做了更改，将列表转换为元组
+                st_list = i.split('\t')
                 
-                # 使用转换后的 label，现在 combination_key 应该不会导致错误了
-                for num in range(start_point, start_point + length):
+                # Check for extra range in 5-column data
+                if len(st_list) == 5:
+                    extra_range = list(map(int, st_list[4].split()))
+                    st_list = st_list[:4]  # 移除额外的列
+                
+                st_join = '\t'.join(info.strip() for info in st_list)
+                target_list = list(range(int(st_list[2]), int(st_list[3])))
+
+                # Add the extra range to target_list
+                if 'extra_range' in locals():
+                    target_list.extend(range(extra_range[0], extra_range[1]))
+                    del extra_range  # 删除变量避免重复使用
+
+                for num in target_list:
                     for name, start, end, loc in data:
                         if start <= num <= end:
-                            combination_key = (start_point, length, label, name)
-                            if combination_key not in printed_combinations:
-                                printed_combinations[combination_key] = True
+                            combination = (tuple(target_list), name, loc)
+                            if combination not in printed_combinations:
                                 ff.write(f"{st_join}\t{name}\t{loc}\n")
+                                printed_combinations.add(combination)
                             break
-    os.remove(output_)
-    os.remove(save_file)
-    print(f"The results was written into:\n\t\t{os.path.abspath(final_results)}\n{'-' * 80}")
+        os.remove(output_)
+        os.remove(save_file)
+        print(f"The results were written into:\n\t\t{os.path.abspath(final_results)}\n{'-' * 80}")
                         
 
 
